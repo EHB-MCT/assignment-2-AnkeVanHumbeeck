7770015529181a75d45d9885b2fe08029b577064
AnkeVanHumbeeck <113995827+AnkeVanHumbeeck@users.noreply.github.com>
2024-11-29 11:46:53 +0100

    refactor: split up formHandler.js for modularity

--- scripts/formHandler.js
+++ scripts/formHandler.js
@@ -1,15 +1,6 @@
 "use strict";
 
-console.log("working")
-// Initialize Supabase client
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://ttssqpywstiupazsmouy.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0c3NxcHl3c3RpdXBhenNtb3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2NjA4MDgsImV4cCI6MjA0NzIzNjgwOH0.CuYqMgZ-NfbNwLNxujpiONU1DTOyGq6KN5qXJgYqc5Q';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-console.log('Supabase initialized:', supabase);
+import { supabase } from './supabaseConnection.js'; 
 
 // Handle form submission
 document.getElementById('youtube-form').addEventListener('submit', async function (event) {
